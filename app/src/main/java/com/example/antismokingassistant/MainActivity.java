4c57bddf216d3c43b220dacd51f9eeb6d544ad86
Jaeyoon <jeyoonchoi03@gmail.com>
2023-11-25 04:21:25 +0900

    1. 권한 오류 떠서 바꾸려고 함 2. (위치, 오디오)권한 완료

--- app/src/main/java/com/example/antismokingassistant/MainActivity.java
+++ app/src/main/java/com/example/antismokingassistant/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.antismokingassistant;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.Manifest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    //권한
+    ActivityResultLauncher<String[]> mPermissionResultLauncher;
+    private boolean isReadPermissionGranted = false;
+    private boolean isLocationPermissionGranted = false;
+    private boolean isRecordPermissionGranted = false;
+
+
+    //
     private Button btn_shoot;
 
     @Override
@@ -16,13 +35,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //권한
+        mPermissionResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), new ActivityResultCallback<Map<String, Boolean>>() {
+            @Override
+            public void onActivityResult(Map<String, Boolean> result) {
 
-        //권한 체크
-        TedPermission.with(getApplicationContext())
-                .setpermissionListener(permissionListener)
-                .setRationaleMessage("카메라 권한이 필요합니다.")
-                .setDeniedMessage("거부하셨습니다.")
-                .setPermissions()
+                if(result.get(Manifest.permission.READ_EXTERNAL_STORAGE) != null){
+                    isReadPermissionGranted = result.get(Manifest.permission.READ_EXTERNAL_STORAGE);
+                }
+
+                if(result.get(Manifest.permission.ACCESS_FINE_LOCATION) != null){
+                    isReadPermissionGranted = result.get(Manifest.permission.ACCESS_FINE_LOCATION);
+                }
+
+                if(result.get(Manifest.permission.RECORD_AUDIO) != null){
+                    isReadPermissionGranted = result.get(Manifest.permission.RECORD_AUDIO);
+                }
+            }
+        });
+
+
+
+
+//        //권한 체크
+//        TedPermission.with(getApplicationContext())
+//                .setpermissionListener(permissionListener)
+//                .setRationaleMessage("카메라 권한이 필요합니다.")
+//                .setDeniedMessage("거부하셨습니다.")
+//                .setPermissions()
 
 
         btn_shoot = findViewById(R.id.btn_shoot);
@@ -34,6 +74,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Step7 : 요청 팝업
+        requestPermission();
+
+
+    }
+
+    //Step4
+    private void requestPermission(){
+
+        isReadPermissionGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED;
+
+        isLocationPermissionGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED;
+
+        isRecordPermissionGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+        ) == PackageManager.PERMISSION_GRANTED;
+
+        //Step5
+        List<String> permissionRequest = new ArrayList<String>();
+
+        if(!isReadPermissionGranted){
+            permissionRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if(!isLocationPermissionGranted){
+            permissionRequest.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if(!isReadPermissionGranted){
+            permissionRequest.add(Manifest.permission.RECORD_AUDIO);
+        }
+
+
+
+        if(!permissionRequest.isEmpty()) {
+            mPermissionResultLauncher.launch(permissionRequest.toArray(new String[0]));
+        }
 
     }
 }
